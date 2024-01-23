c3b04a138fd16a31743fc963e14d31033f448061
sergeykraynov <kraynov.sergey@gmail.com>
2024-01-23 17:59:57 +0300

    Add new cursor

--- assets/theme/chunks/footer.tpl
+++ assets/theme/chunks/footer.tpl
@@ -47,4 +47,15 @@
     </div>
 </footer>
 
-<div class="cursor" data-cursor></div>
+<div class="cursor" id="cursor" style="filter: url('#goo');"></div>
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="800">
+    <defs>
+        <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur"></feGaussianBlur>
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 35 -15" result="goo">
+            </feColorMatrix>
+            <feComposite in="SourceGraphic" in2="goo" operator="atop"></feComposite>
+        </filter>
+    </defs>
+</svg>
--- dist/js/app.js
+++ dist/js/app.js
@@ -39426,7 +39426,7 @@ if (cases_hover.length > 0) {
     }
 }
 
-const cursor = document.querySelector('[data-cursor]');
+/*const cursor = document.querySelector('[data-cursor]');
 if ( cursor ) {
     let posX = 0,
         posY = 0,
@@ -39437,7 +39437,7 @@ if ( cursor ) {
         posX += (mouseX - posX) / 9;
         posY += (mouseY - posY) / 9;
 
-        gsapWithCSS.set(cursor, {
+        gsap.set(cursor, {
             css: {
                 left: posX,
                 top: posY
@@ -39449,7 +39449,7 @@ if ( cursor ) {
         mouseX = e.pageX;
         mouseY = e.pageY;
     });
-}
+}*/
 
 
 /* 100vh на мобильных устройствах */
@@ -39719,6 +39719,121 @@ if (budget_inputs) {
         });
     });
 }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; } const cursor = document.getElementById("cursor");
+const amount = 20;
+const sineDots = Math.floor(amount * 0.3);
+const width = 26;
+const idleTimeout = 150;
+let lastFrame = 0;
+let mousePosition = { x: 0, y: 0 };
+let dots = [];
+let timeoutID;
+let idle = false;
+
+class Dot {
+    constructor(index = 0) {
+        this.index = index;
+        this.anglespeed = 0.05;
+        this.x = 0;
+        this.y = 0;
+        this.scale = 1 - 0.05 * index;
+        this.range = width / 2 - width / 2 * this.scale + 2;
+        this.limit = width * 0.75 * this.scale;
+        this.element = document.createElement("span");
+        gsapWithCSS.set(this.element, { scale: this.scale });
+        cursor.appendChild(this.element);
+    }
+
+    lock() {
+        this.lockX = this.x;
+        this.lockY = this.y;
+        this.angleX = Math.PI * 2 * Math.random();
+        this.angleY = Math.PI * 2 * Math.random();
+    }
+
+    draw(delta) {
+        if (!idle || this.index <= sineDots) {
+            gsapWithCSS.set(this.element, { x: this.x, y: this.y });
+        } else {
+            this.angleX += this.anglespeed;
+            this.angleY += this.anglespeed;
+            this.y = this.lockY + Math.sin(this.angleY) * this.range;
+            this.x = this.lockX + Math.sin(this.angleX) * this.range;
+            gsapWithCSS.set(this.element, { x: this.x, y: this.y });
+        }
+    }
+}
+
+function init() {
+    window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("touchmove", app_onTouchMove);
+    lastFrame += new Date();
+    buildDots();
+    render();
+}
+
+function startIdleTimer() {
+    timeoutID = setTimeout(goInactive, idleTimeout);
+    idle = false;
+}
+
+function resetIdleTimer() {
+    clearTimeout(timeoutID);
+    startIdleTimer();
+}
+
+function goInactive() {
+    idle = true;
+    for (let dot of dots) {
+        dot.lock();
+    }
+}
+
+function buildDots() {
+    for (let i = 0; i < amount; i++) {
+        let dot = new Dot(i);
+        dots.push(dot);
+    }
+}
+
+const onMouseMove = event => {
+    mousePosition.x = event.clientX - width / 2;
+    mousePosition.y = event.clientY - width / 2;
+    resetIdleTimer();
+};
+
+const app_onTouchMove = () => {
+    mousePosition.x = event.touches[0].clientX - width / 2;
+    mousePosition.y = event.touches[0].clientY - width / 2;
+    resetIdleTimer();
+};
+
+const render = timestamp => {
+    const delta = timestamp - lastFrame;
+    positionCursor(delta);
+    lastFrame = timestamp;
+    requestAnimationFrame(render);
+};
+
+const positionCursor = delta => {
+    let x = mousePosition.x;
+    let y = mousePosition.y;
+    dots.forEach((dot, index, dots) => {
+        let nextDot = dots[index + 1] || dots[0];
+        dot.x = x;
+        dot.y = y;
+        dot.draw(delta);
+        if (!idle || index <= sineDots) {
+            const dx = (nextDot.x - dot.x) * 0.35;
+            const dy = (nextDot.y - dot.y) * 0.35;
+            x += dx;
+            y += dy;
+        }
+    });
+};
+
+init();
 
 document.addEventListener('fetchit:success', (e) => { 
     Jt.close();
